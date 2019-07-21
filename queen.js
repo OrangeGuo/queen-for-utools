44f1500302e07515ebe549d061dfbe259d21e9ef
OrangeGuo <1772086465@qq.com>
2019-07-21 18:55:15 +0800

    start war one

--- queen.js
+++ queen.js
@@ -8,33 +8,48 @@ window.onload = function () {
     ctx = cs.getContext('2d');//context对象
     ctx.fillStyle = '#bae7ff';
     ctx.fillRect(520, 0, 180, 520);
-    ctx.fillStyle = '#ff85c0'
-    initCanvas(ctx)
+    ctx.fillStyle = '#fcffe6';
+    //initCanvas(ctx)
     stack.push(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height));
-    cs.addEventListener('mousemove', function (e) {
-        ctx.fillStyle = '#f5222d';
-        ctx.save();
-        setQueen(ctx, (e.clientX - e.clientX % size) / size, (e.clientY - e.clientY % size) / size);
-    });
-
+    //cs.addEventListener('mousemove', function (e) {
+    //    setQueen((e.clientX - e.clientX % size) / size, (e.clientY - e.clientY % size) / size);
+    //});
 
 }
-function initCanvas(pen) {
+function initCanvas() {
     for (var i = 0; i <= num; i++) {
-        pen.moveTo(0, i * size);
-        pen.lineTo(size * num, i * size);
-        pen.stroke();
+        ctx.moveTo(0, i * size);
+        ctx.lineTo(size * num, i * size);
+        ctx.stroke();
     }
     for (var i = 0; i <= num; i++) {
-        pen.moveTo(i * size, 0);
-        pen.lineTo(i * size, size * num);
-        pen.stroke();
+        ctx.moveTo(i * size, 0);
+        ctx.lineTo(i * size, size * num);
+        ctx.stroke();
     }
 }
-function setQueen(pen, x, y) {
+function setQueen(x, y) {
     if (x > 7 || y > 7)
         return;
     ctx.putImageData(stack[0], 0, 0);
 
-    pen.fillRect(x * size, y * size, size, size)
+    ctx.fillStyle = '#ffccc7';
+    var d=y-x;
+    var s=x+y;
+    for(var i=0;i<num;i++){
+        if(d+i<0||d+i>7)
+            continue;
+        ctx.fillRect(i*size,(d+i)*size,size,size);
+    }
+    for(var i=0;i<num;i++){
+        if(s-i<0||s-i>7)
+            continue;
+        ctx.fillRect(i*size,(s-i)*size,size,size);
+    }
+    ctx.fillStyle = '#f5222d';
+    ctx.fillRect(x * size, y * size, size, size);
+}
+
+function war_1(){
+    
 }
